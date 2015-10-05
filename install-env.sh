a0895243605966130fc256062f83b5792f1b330a
usneha <usneha@hawk.iit.edu>
2015-10-04 22:02:14 -0500

    added script for environment setup

---
+++ install-env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get install -y apache2 git
+
+git clone https://github.com/usneha/itmo-544-444-fall-2015.git
+
+mv ./itmo-544-444-fall-2015/index.html /var/www/html
+
+echo "Hello" > /tmp/hello.txt
+
