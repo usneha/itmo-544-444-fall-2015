90f2baba60bf6004e52300c3e8725cc1bd42bb7c
usneha <usneha@hawk.iit.edu>
2015-10-25 22:11:33 -0500

    re commiting index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 
 print 'Hello World';
 
@@ -14,21 +13,3 @@ Hello <? print 'world'; ?>
 </body>
 
 </html>
-
-=======
-print 'Hello World';
-
-
-?>
-
-<html>
-<head><title> This is my MP1 index.php page </title>
-</head>
-
-<body>
-
-Hello <?php print 'world'; ?>
-
-</body>
-</html>
->>>>>>> 018517e1693cfa74d259fa7dd355d1a029622f90
